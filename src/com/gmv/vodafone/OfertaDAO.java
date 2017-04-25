fef603df455a2dcdf743aac471897397e8e3cd5b
crischoque <cristinachoque3@gmail.com>
2017-04-25 10:33:20 +0200

    modificacion añadir localdate

--- src/com/gmv/vodafone/OfertaDAO.java
+++ src/com/gmv/vodafone/OfertaDAO.java
@@ -1,5 +1,6 @@
 package com.gmv.vodafone;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface OfertaDAO {
@@ -7,5 +8,7 @@ public interface OfertaDAO {
 	List<Oferta> getOfertas() throws OfertaException;
 	
 	Oferta getOferta(int codigoOferta) throws OfertaException;
+	
+	LocalDate getOfertaByDate(int codigoOferta);
 
 }
